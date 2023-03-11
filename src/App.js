e5d30f2938971595c1951e677b9046badf53b1d9
jiho9118 <mexocoxon@gmail.com>
2023-03-11 23:52:06 +0900

    structure setting

--- src/App.js
+++ src/App.js
@@ -2,7 +2,172 @@ import './App.css';
 
 function App() {
   return (
-    <></>
+    <>
+      <header>
+        <nav>
+          <img src='' alt='logo'/>
+          <ul>
+            <li>Funrniture</li>
+            <li>Shop</li>
+            <li>About Us</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <div></div> {/* 배경용 div */}
+        <h1>Make Your Interior More Minimaistic & Modern</h1>
+        <p>
+          Bringing interiors to life, understanding the person or people living
+          in a home is far greater need in design.
+        </p>
+        <input placeholder='Search Furniture' />
+      </section>
+      <section>
+        <h2>Why Choosing Us</h2>
+        <div>content area</div>
+        <div>content area</div>
+        <div>content area</div>
+        <div>content area</div>
+      </section>
+      <section>
+        <h2>Visualize your ideas with us</h2>
+        <p>
+          We are ready to help you build and also realize the room design that
+          you dream of, with our experts and also the best category
+          recommendations from us
+        </p>
+        <details>
+          <summary>Modern</summary>
+          Modern design is a term in the design world that refers to a house
+          with a clean design, simple color choices, the use of materials
+          including metal, glass, and steel, and is usually accompanied by clean
+          lines.
+        </details>
+        <details>
+          <summary>Contemporary</summary>
+          Modern design is a term in the design world that refers to a house
+          with a clean design, simple color choices, the use of materials
+          including metal, glass, and steel, and is usually accompanied by clean
+          lines.
+        </details>
+        <details>
+          <summary>Minimalis</summary>
+          Modern design is a term in the design world that refers to a house
+          with a clean design, simple color choices, the use of materials
+          including metal, glass, and steel, and is usually accompanied by clean
+          lines.
+        </details>
+        <details>
+          <summary>Industrial</summary>
+          Modern design is a term in the design world that refers to a house
+          with a clean design, simple color choices, the use of materials
+          including metal, glass, and steel, and is usually accompanied by clean
+          lines.
+        </details>
+        <p>See more catagory</p>
+        <img src='' />
+      </section>
+      <section>
+        <h2>Featured Product</h2>
+        <div>Product area</div>
+        <div>Product area</div>
+        <div>Product area</div>
+        <div>Product area</div>
+      </section>
+      <section>
+        <h2>What they say about our services</h2>
+        <div>review area</div>
+        <div>review area</div>
+        <div>review area</div>
+      </section>
+      <section>
+        <img src='' />
+        <h2>Make your room feel homey</h2>
+        <p>
+          We help people not only to make the room at home better, but we help
+          people to feel a more homey and comfortable room, you don’t have to
+          worry about the result because of these interiors are made by people
+          who are professionals in their dields with an elegant and lucuriois
+          style and with premium quality materials Contact us if you are
+          interested in working with us
+        </p>
+        <input placeholder='Insert your email address' />
+      </section>
+
+      <footer>
+        <section>
+          <img src='' alt='logo' />
+          <div>
+            <h2>Features</h2>
+            <ul>
+              <li>
+                <a href='#'>Our Features</a>
+              </li>
+              <li>
+                <a href='#'>Affiliate Program</a>
+              </li>
+              <li>
+                <a href='#'>Press Kit</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Support</h2>
+            <ul>
+              <li>
+                <a href='#'>Account</a>
+              </li>
+              <li>
+                <a href='#'>Help</a>
+              </li>
+              <li>
+                <a href='#'>Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Legal</h2>
+            <ul>
+              <li>
+                <a href='#'>Privacy Policy</a>
+              </li>
+              <li>
+                <a href='#'>Terms of Use</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Social Media</h2>
+            <ul>
+              <li>
+                <a href='#'>Twitter</a>
+              </li>
+              <li>
+                <a href='#'>Product Hunt</a>
+              </li>
+              <li>
+                <a href='#'>Instagram</a>
+              </li>
+              <li>
+                <a href='#'>Github</a>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section>
+          <ul>
+            <li>
+              <a href='#'>Terms &amp; Conditions</a>
+            </li>
+            <li>
+              <a href='#'>Privacy Policy</a>
+            </li>
+            <li>&copy; 2019 Copyright Nowrap Inc.</li>
+          </ul>
+        </section>
+      </footer>
+    </>
   );
 }
 
